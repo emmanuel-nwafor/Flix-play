255a06bbf4b467048119aa1eccaffde33996dc25
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-10 04:29:13 -0700

    all commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
 import { Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#1b1b1b",
-      }}
-    >
-      <Text style={{ color: "white", }}>Home Screen hio</Text>
-    </View>
+        <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1b1b1b",
+        }}>
+          <Text style={{ color: "white" }}>Home Screen hio</Text>
+        </View>
   );
 }
