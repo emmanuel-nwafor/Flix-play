dfbe57c8c26d3d11e85d29898a18f54dfa888e46
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-10 04:51:53 -0700

    all commit

--- app/(tabs)/me.tsx
+++ app/(tabs)/me.tsx
@@ -2,7 +2,12 @@ import { View, Text } from 'react-native'
 
 export default function me() {
   return (
-    <View>
+    <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#1b1b1b",
+      }}>
       <Text>
         Hello Me
       </Text>
--- app/(tabs)/shelf.tsx
+++ app/(tabs)/shelf.tsx
@@ -2,7 +2,12 @@ import { View, Text } from 'react-native'
 
 export default function shelf() {
   return (
-    <View>
+    <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#1b1b1b",
+      }}>
       <Text>
         Hello Shelf
       </Text>
